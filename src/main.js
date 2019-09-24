577e98cff4cd07c63392a3f0067768d42b0980bc
WolfGirlM <18829491124@163.com>
2019-09-24 18:16:16 +0800

    Echarts&Vue第一节

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import echarts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.use(echarts)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/simpleCharts/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <p> Echarts demos</p>
+    <div ref="echarts" class="simpleDemo">
+    </div>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+  export default {
+    data() {
+      return {
+        chart: null,
+        option:  {
+            title: {
+                text: 'Biu~简单柱状图示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20],
+                 color: ['#66FF99']
+            }],
+            
+        }
+      }
+    },
+    mounted() {
+      this.getPage()
+    },
+    methods: {
+      getPage() {
+        this.chart = echarts.init(this.$refs.echarts);
+        // 使用刚指定的配置项和数据显示图表。
+        this.chart.setOption(this.option);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.simpleDemo {
+  width: 600px;
+  height:400px;
+  margin: 0 auto;
+}
+</style>
+
